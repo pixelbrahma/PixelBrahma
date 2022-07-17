f01786bc736b2f7a1d0c3beac02421dd7d5a52a8
Nikhil Bharadwaj <44049892+pixelbrahma@users.noreply.github.com>
2022-07-17 19:20:16 +0100

    Rendered first triangle on the screen

--- PixelBrahma/src/PixelBrahma/Application/Application.cpp
+++ PixelBrahma/src/PixelBrahma/Application/Application.cpp
@@ -26,6 +26,39 @@ namespace PixelBrahma
 		// Create ImGui layer push it to the layer stack
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		// Generate and bind vertex array
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+
+		// Generate aand bind vertex buffer
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+		// Vertex positions array
+		float vertices[3 * 3] =
+		{
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f,  0.5f, 0.0f
+		};
+
+		// Assign vertex buffer data
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		// Vertex attribute arrray and pointer
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		// Generate and bind index buffer
+		glGenBuffers(1, &m_IndexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		// Indices array
+		unsigned int indices[3] = { 0, 1, 2 };
+
+		// Assign index buffer data
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	}
 
 	Application::~Application() {}
@@ -61,8 +94,12 @@ namespace PixelBrahma
 		while (m_Running)
 		{
 			// Clear the color buffer
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			// Draw the elements of the vertices array
+			glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 			// Update each layer in order
 			for (Layer* layer : m_LayerStack)
--- PixelBrahma/src/Platform/OpenGL/OpenGLContext.cpp
+++ PixelBrahma/src/Platform/OpenGL/OpenGLContext.cpp
@@ -15,9 +15,18 @@ namespace PixelBrahma
 	// Initialize OpenGL context
 	void OpenGLContext::Init()
 	{
+		// Set window context
 		glfwMakeContextCurrent(m_WindowHandle);
+
+		// Initialize glad
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		PB_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		// Print out OpenGL information
+		PB_CORE_INFO("OpenGL Info:");
+		PB_CORE_INFO(" Vendor: {0}", glGetString(GL_VENDOR));
+		PB_CORE_INFO(" Renderer: {0}", glGetString(GL_RENDERER));
+		PB_CORE_INFO(" Version: {0}", glGetString(GL_VERSION));
 	}
 
 	// Swap OpenGL graphics bufffers
